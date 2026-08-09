67b88a2a8fb3d87816722d7bcb3466c11652a455
Marco Bidoli <marco.bidoli@gmail.com>
2026-08-09 20:45:01 +0200

    Add /fields/:id/bookings route

--- app/routes/fields.js
+++ app/routes/fields.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {getDB} from "../db.js";
 import {ObjectId} from "mongodb";
+import passport from "passport";
 
 const router = express.Router();
 
@@ -64,6 +65,48 @@ router.get("/:id/slots", async (req, res, next) => {
 
         res.json(availableSlots);
     } catch (err) {
+        next(err);
+    }
+});
+
+router.post("/:id/bookings", passport.authenticate("jwt", { session: false }), async (req, res, next) => {
+    try {
+        const {date, slot, type = "standard"} = req.body;
+        const fieldId = new ObjectId(req.params.id);
+        const db = getDB();
+
+        // Field existence
+        const field = await db.collection("fields").findOne({_id: new ObjectId(fieldId)});
+        if (!field || !field.slots.includes(slot)) {
+            return res.status(400).json({error: "Invalid field or slot"});
+        }
+
+        // future booking
+        const bookingDateTime = new Date(`${date}T${slot.split('-')[0]}`);
+        if (bookingDateTime <= Date.now()) {
+           return res.status(400).json({error: "Booking date must be in the future"});
+        }
+
+        // insert booking
+        const newBooking = {
+            fieldId,
+            userId: req.user._id, // set by passport-jwt
+            date,
+            slot,
+            type, // normal or tournament only
+            createdAt: new Date()
+        }
+
+        const result = await db.collection("bookings").insertOne(newBooking);
+        res.status(201).json({
+            _id: result.insertedId.toString(),
+            ...newBooking
+        });
+    } catch (err) {
+        // MongoDB compound unique index violation
+        if(err.code === 11000) {
+            return res.status(409).json({error: "This slot is already booked"});
+        }
         next(err);
     }
 });
